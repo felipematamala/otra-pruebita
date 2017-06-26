4a140f0cc06aa1a87b975a7a017157420decc861
felipematamala <matatore346@gmail.com>
2017-06-26 11:18:11 -0300

    -a

---
+++ factura-master/src/factura/Factura.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factura;
+
+import java.util.ArrayList; 
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+
+
+/**
+ *
+ * @author alumno
+ */
+public class Factura {
+
+    private ArrayList<ItemFactura> itemFactura = new ArrayList();    
+    private FormaCobro formaCobro;
+    private Cliente cliente;
+    private Date fecha;
+    private Integer numero;
+
+    public ArrayList<ItemFactura> getItemFactura() {
+        return itemFactura;
+    }
+    public void agregarItem(ItemFactura pItemFactura){
+       itemFactura.add(pItemFactura);
+    }
+    
+    
+
+    public FormaCobro getFormaCobro() {
+        return formaCobro;
+    }
+
+    public void setFormaCobro(FormaCobro formaCobro) {
+        this.formaCobro = formaCobro;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+    
+       
+   
+}
---
+++ src/factura/Cliente.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factura;
+
+/**
+ *
+ * @author alumno
+ */
+public class Cliente {
+    private String nombre;
+    private String apellido;
+    private Integer DNI;
+    private String calle;
+    private String altitud;
+private Localidad localidad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Integer getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(Integer DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getAltitud() {
+        return altitud;
+    }
+
+    public void setAltitud(String altitud) {
+        this.altitud = altitud;
+    }
+
+    public Localidad getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(Localidad localidad) {
+        this.localidad = localidad;
+    }
+
+
+}
